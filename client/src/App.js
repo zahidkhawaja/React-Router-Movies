6828dc2e0f28eb2942fff02f58f7185c13e9ba6b
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-24 15:40:33 -0500

    Add route (/) for MovieList and dynamic route (id) for Movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +12,13 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = "/">
+        <MovieList />
+      </Route>
+
+      <Route path = "/movies/:id">
+        <Movie />
+      </Route>
     </div>
   );
 };
