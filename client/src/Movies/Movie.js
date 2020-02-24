7378a6c01ae97780f241b9a42ef8167840e8a25d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-24 17:46:40 -0500

    Complete dynamic links using useParams hook in Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const  { id } = useParams();
+  
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
