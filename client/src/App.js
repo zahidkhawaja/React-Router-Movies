620a11bc5f013eb904f10b28190afb767e016380
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-24 15:42:45 -0500

    Import Movie and Movielist in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
