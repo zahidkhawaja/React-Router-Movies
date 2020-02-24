8d76e613f12bc04b7e531ac9d8112924e217d711
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-24 16:01:22 -0500

    Add Link component (/) to SavedList.js for home button

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,7 +7,9 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div className="home-button">
+      <Link to = "/">Home</Link>
+    </div>
   </div>
 );
 
