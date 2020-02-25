0d602f50088fe26888aa37680c28c2024c8dc238
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-25 16:53:05 -0500

    Turn saved movie 'Links' to 'NavLinks'

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">
-      <Link to = {`/movies/${movie.id}`}>{movie.title}</Link>
+      <NavLink to = {`/movies/${movie.id}`}>{movie.title}</NavLink>
       </span>
     ))}
     <div className="home-button">
